0a1c89f7030669f580760f36087562c258852a9b
shortword1 <zackkinsman@gmail.com>
2025-05-21 11:38:50 -0300

    Updates

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './' // Ensure assets are loaded with relative paths
-})
